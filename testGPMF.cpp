c8e69c1dab1819e5f238469b6753ae0a56af118a
faalbers <chalbers@gmail.com>
2021-09-02 10:11:31 -0700

    time test

--- testGPMF.cpp
+++ testGPMF.cpp
@@ -1,27 +1,36 @@
 #include "GPMF.cpp"
 #include <iostream>
 #include <iomanip>
-#include <fstream>
+#include <chrono>
+
 
 int main(int argc, char* argv[])
 {
     if ( argc == 1 ) return 1;
+    
+    auto createStart = std::chrono::high_resolution_clock::now();
 
     std::cout << "**** CREATE GPMF ****\n\n";
 
     GPMF::GPMF gpmf(argv[1]);
 
+    auto testStart = std::chrono::high_resolution_clock::now();
+    auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(testStart - createStart);
+    std::cout << "Create Time: " << ms_int.count() << "ms\n";
+    
     std::cout << "\n**** TEST GPMF ****\n\n";
     
     //gpmf.printHierarchy();
     
     //gpmf.printHierarchyData();
 
-    if (true) {
-        //auto sampels = gpmf.getAcceleration();
+    auto sampels = gpmf.getAcceleration();
+    std::cout << sampels.size() << std::endl;
+    
+    if (false) {
+        auto sampels = gpmf.getAcceleration();
         //auto sampels = gpmf.getGyroscope();
-        auto sampels = gpmf.getGPS();
-        std::cout << sampels.size() << std::endl;
+        //auto sampels = gpmf.getGPS();
         std::cout.precision(5);
         std::cout << std::fixed;
         int fixedWith = 10;
@@ -37,6 +46,10 @@ int main(int argc, char* argv[])
     }
 
     //gpmf.exportGPStoGPX("Blah");
+
+    auto end = std::chrono::high_resolution_clock::now();
+    ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(end - testStart);
+    std::cout << "Test Time: " << ms_int.count() << "ms\n";
 
     std::cout << "\n**** END ****\n\n";
 
